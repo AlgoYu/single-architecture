a9e1f0d5ab38f9ed68a5732bfb4867b927ced885
MachineGeek <794763733@qq.com>
2021-01-13 12:40:29 +0800

    update sql file.

--- single_architecture.sql
+++ single_architecture.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 06/01/2021 18:58:43
+ Date: 13/01/2021 12:39:36
 */
 
 SET NAMES utf8mb4;
@@ -40,7 +40,7 @@ CREATE TABLE `account` (
 -- Records of account
 -- ----------------------------
 BEGIN;
-INSERT INTO `account` VALUES (1, 'admin', '$2a$10$L7AMqBhiwRrCVNPSMbxIqOerf1WBTFjVDE24S.x2x.ZbofNxUNPii', '127.0.0.1', 1, 0, '2021-01-06 13:41:43', '2021-01-06 13:41:45', '2021-01-06 13:41:46');
+INSERT INTO `account` VALUES (1347107263809724418, 'admin', '$2a$10$L7AMqBhiwRrCVNPSMbxIqOerf1WBTFjVDE24S.x2x.ZbofNxUNPii', '127.0.0.1', 1, 0, '2021-01-06 13:41:43', '2021-01-06 13:41:45', '2021-01-06 13:41:46');
 COMMIT;
 
 -- ----------------------------
@@ -61,7 +61,7 @@ CREATE TABLE `account_role_relation` (
 -- Records of account_role_relation
 -- ----------------------------
 BEGIN;
-INSERT INTO `account_role_relation` VALUES (1, 1);
+INSERT INTO `account_role_relation` VALUES (1347107263809724418, 1347107367358693378);
 COMMIT;
 
 -- ----------------------------
@@ -72,6 +72,9 @@ CREATE TABLE `authority` (
   `id` bigint NOT NULL COMMENT '唯一标识',
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '名称',
   `key` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '关键字',
+  `pid` bigint DEFAULT NULL COMMENT '父权力ID',
+  `sort` int DEFAULT NULL COMMENT '排序',
+  `uri` varchar(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'URI',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `authority_id_uindex` (`id`) USING BTREE,
   UNIQUE KEY `authority_key_uindex` (`key`) USING BTREE,
@@ -82,7 +85,29 @@ CREATE TABLE `authority` (
 -- Records of authority
 -- ----------------------------
 BEGIN;
-INSERT INTO `authority` VALUES (1, '权限1', 'auth1');
+INSERT INTO `authority` VALUES (1348514149771689985, '账号列表', 'ACCOUNT', 1348542368843452418, 0, '/Account');
+INSERT INTO `authority` VALUES (1348514149771689986, '增加账号', 'ACCOUNT:ADD', 1348514149771689985, 0, NULL);
+INSERT INTO `authority` VALUES (1348514149771689987, '删除账号', 'ACCOUNT:DELETE', 1348514149771689985, 0, NULL);
+INSERT INTO `authority` VALUES (1348514149771689988, '修改账号', 'ACCOUNT:MODIFY', 1348514149771689985, 0, NULL);
+INSERT INTO `authority` VALUES (1348514149771689989, '查询账号', 'ACCOUNT:GET', 1348514149771689985, 0, NULL);
+INSERT INTO `authority` VALUES (1348516637811826690, '角色列表', 'ROLE', 1348542368843452418, 0, '/Role');
+INSERT INTO `authority` VALUES (1348516637811826691, '增加角色', 'ROLE:ADD', 1348516637811826690, 0, NULL);
+INSERT INTO `authority` VALUES (1348516637811826692, '删除角色', 'ROLE:DELETE', 1348516637811826690, 0, NULL);
+INSERT INTO `authority` VALUES (1348516637811826693, '修改角色', 'ROLE:MODIFY', 1348516637811826690, 0, NULL);
+INSERT INTO `authority` VALUES (1348516637811826694, '查询角色', 'ROLE:GET', 1348516637811826690, 0, NULL);
+INSERT INTO `authority` VALUES (1348517662148952066, '权限列表', 'AUTHORITY', 1348542368843452418, 0, '/Authority');
+INSERT INTO `authority` VALUES (1348517662148952067, '增加权限', 'AUTHORITY:ADD', 1348517662148952066, 0, NULL);
+INSERT INTO `authority` VALUES (1348517662148952068, '删除权限', 'AUTHORITY:DELETE', 1348517662148952066, 0, NULL);
+INSERT INTO `authority` VALUES (1348517662148952069, '修改权限', 'AUTHORITY:MODIFY', 1348517662148952066, 0, NULL);
+INSERT INTO `authority` VALUES (1348517662148952070, '查询权限', 'AUTHORITY:GET', 1348517662148952066, 0, NULL);
+INSERT INTO `authority` VALUES (1348542368843452418, '系统管理', 'SYSTEM', 0, 0, '/System');
+INSERT INTO `authority` VALUES (1348542368843452419, '开发者工具', 'DEVTOOLS', 0, 0, '/DevTools');
+INSERT INTO `authority` VALUES (1348542368843452420, '系统设置', 'SETTING', 0, 0, '/Setting');
+INSERT INTO `authority` VALUES (1348542368843452421, '数据中心', 'DASHBOARD', 0, 1, '/Dashboard');
+INSERT INTO `authority` VALUES (1348542368843452422, '代码生成器', 'CODEGENERATOR', 1348542368843452419, 0, '/CodeGenerator');
+INSERT INTO `authority` VALUES (1348542368843452423, '数据监控', 'MONITOR', 1348542368843452419, 0, '/Monitor');
+INSERT INTO `authority` VALUES (1348542368843452424, '异常信息', 'EXCEPTION', 1348542368843452419, 0, '/Exception');
+INSERT INTO `authority` VALUES (1348542368843452425, '开发文档', 'DEVDOC', 1348542368843452419, 0, '/DevDoc');
 COMMIT;
 
 -- ----------------------------
@@ -106,7 +131,7 @@ CREATE TABLE `role` (
 -- Records of role
 -- ----------------------------
 BEGIN;
-INSERT INTO `role` VALUES (1, '超级管理员', 'ROLE_Administrator', 0, '2021-01-06 13:54:03', '2021-01-06 13:54:05');
+INSERT INTO `role` VALUES (1347107367358693378, '超级管理员', 'ROLE_Administrator', 0, '2021-01-06 13:54:03', '2021-01-06 13:54:05');
 COMMIT;
 
 -- ----------------------------
@@ -127,7 +152,10 @@ CREATE TABLE `role_authority_relation` (
 -- Records of role_authority_relation
 -- ----------------------------
 BEGIN;
-INSERT INTO `role_authority_relation` VALUES (1, 1);
+INSERT INTO `role_authority_relation` VALUES (1347107367358693378, 1348514149771689985);
+INSERT INTO `role_authority_relation` VALUES (1347107367358693378, 1348516637811826690);
+INSERT INTO `role_authority_relation` VALUES (1347107367358693378, 1348516637811826692);
+INSERT INTO `role_authority_relation` VALUES (1347107367358693378, 1348542368843452418);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
